1ce6c33c3dfc26d8f97c2faf4c08014770bd2cf3
John Murray <jmurray@redventures.com>
2015-03-23 03:04:17 -0400

    beging to set up sticky sections

--- js/project.js
+++ js/project.js
@@ -3,5 +3,13 @@ $(function() {
 		strings: ['Designer', 'Developer', 'UX Expert', 'Strategist', 'Husband', 'Father', 'Leader', 'Mentor', 'Southerner'],
         typeSpeed: 100
 	});
+
+	$(window).scroll(function(){
+	  	if ($(this).scrollTop() > 1350){
+			$('.work sidebar').addClass('is-sticky');
+		} else {
+			$('.work sidebar').removeClass('is-sticky');
+		}	
+	});
 //close jquery    
 });
