a6e788cc79a5699bdab1400b16dc205c9b03cc8d
John Murray <jmurray@redventures.com>
2015-04-26 23:23:13 -0400

    not worrying about IE 8 foundation support

--- assets/includes/base/footer.php
+++ assets/includes/base/footer.php
@@ -1,4 +1,3 @@
-<script src="assets/js/plugins/rem.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/unveil/1.3.0/jquery.unveil.min.js"></script>
 <script src="assets/js/project.min.js"></script>
--- assets/includes/base/header.php
+++ assets/includes/base/header.php
@@ -12,10 +12,4 @@
 
 <link rel="stylesheet" href="assets/css/project.min.css">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
-<!--[if lt IE 9]>
-	<script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.6.2/html5shiv.js"></script>
-	<script src="//s3.amazonaws.com/nwapi/nwmatcher/nwmatcher-1.2.5-min.js"></script>
-	<script src="//html5base.googlecode.com/svn-history/r38/trunk/js/selectivizr-1.0.3b.js"></script>
-	<script src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.1.0/respond.min.js"></script>
-<![endif]-->
 </head>
