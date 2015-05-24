136f20f81f74a74272a3785a99dcdb045c723e69
John Murray <jmurray@redventures.com>
2015-05-23 23:55:01 -0400

    add process outline

--- process.php
+++ process.php
@@ -8,7 +8,7 @@
 
 				<div class="headline"><span>About My</span> UX Design Process</div>
 				
-				<p>With few exceptions, my design consistently follows the same steps:</p>
+				<p>Like most UX designers, my process is as follows:</p>
 
 				<ol class="list has-row-colors">
 					<li>Initial planning</li>
@@ -26,10 +26,82 @@
 		</aside>
 		<div class="content align-right">
 			<div class="body-copy">
-				<h1>header</h1>
-				<h2>subheader</h2>
+				<div class="row">
+					<div class="column small-12">
+						<h1>Before we begin ...</h1>
+						<p>I'm going to describe my process using a web app that I'm currently building with EmberJS. Although, I'm proud of this project, it doesn't show up in my portfolio because it's not complete. If you're interested in seeing the front end code for it, then <a href="https://github.com/heyjohnmurray/EmberApp">check it out on my github.</a></p>
+					</div>
+					<!-- /.column -->
+				</div>
+				<!-- /.row -->
 			</div>
 			<!-- /.body-copy -->
+
+			<div class="body-copy">
+				<div class="row">
+					<div class="column">
+						<h2>Step One: Initial Planning</h2>
+						<h3>Define the problem</h3>
+						<h3>Describe the ideal user</h3>
+						<p></p>
+					</div>
+					<!-- /.column -->
+				</div>
+				<!-- /.row -->
+			</div>
+			<!-- /.body-copy -->
+
+			<div class="body-copy">
+				<div class="row">
+					<div class="column">
+						<h2>Step Two: Wireframes</h2>
+						<h3>User events</h3>
+						<h3>Initial sketches</h3>
+						<h3>Project map</h3>
+						<p>"One thing I picked up over recent years that it's very tempting to try and sell your ideas to stakeholders by making the material look good. It should be clear, but it should be easily criticised and invite comments. Adding too much polish at this stage can start to win people over rather than inviting them to look-under-the-hood and and judge the concept rather than the execution."</p>
+					</div>
+					<!-- /.column -->
+				</div>
+				<!-- /.row -->
+			</div>
+			<!-- /.body-copy -->
+
+			<div class="body-copy">
+				<div class="row">
+					<div class="column">
+						<h2>Step Three: Prototypes</h2>
+						<p></p>
+					</div>
+					<!-- /.column -->
+				</div>
+				<!-- /.row -->
+			</div>
+			<!-- /.body-copy -->
+
+			<div class="body-copy">
+				<div class="row">
+					<div class="column">
+						<h2>Step Four: Initial Mockups</h2>
+						<p></p>
+					</div>
+					<!-- /.column -->
+				</div>
+				<!-- /.row -->
+			</div>
+			<!-- /.body-copy -->
+
+			<div class="body-copy">
+				<div class="row">
+					<div class="column">
+						<h2>Step Five: Detailed Mockups</h2>
+						<p></p>
+					</div>
+					<!-- /.column -->
+				</div>
+				<!-- /.row -->
+			</div>
+			<!-- /.body-copy -->
+
 		</div>
 		<!-- /.content -->
 	</section>
