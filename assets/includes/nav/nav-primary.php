c5d6848e83fb1199749e68ae674675e2a3ab23a1
John Murray <heyjohnmurray@us.ibm.com>
2018-04-01 00:26:12 -0500

    fix relative urls for site pages

--- assets/includes/nav/nav-primary.php
+++ assets/includes/nav/nav-primary.php
@@ -3,19 +3,19 @@
 		<li><a href="/" class="nav-close js-nav-close"><i class="icon-cancel"></i> Close</a></li>
 	<? if(stristr($_SERVER['REQUEST_URI'], "/index.php")) { ?>
 		<li><a href="/">Home</a></li>
-		<li><a href="#portfolio-section">Portfolio</a></li>
+		<li><a href="/#portfolio-section">Portfolio</a></li>
 		<li><a href="#about-section">About</a></li>
-		<li><a href="design-leadership.php">Design Leadership</a></li>
-		<li><a href="design-thinking.php">Design Thinking</a></li>
-		<li><a href="design-mentorship.php">Mentorship</a></li>
-		<li><a href="#contact-section">Contact</a></li>
+		<li><a href="/design-leadership.php">Design Leadership</a></li>
+		<li><a href="/design-thinking.php">Design Thinking</a></li>
+		<li><a href="/design-mentorship.php">Mentorship</a></li>
+		<li><a href="/#contact-section">Contact</a></li>
 	<? } else { ?>
 		<li><a href="/">Home</a></li>
-		<li><a href="index.php#portfolio-section">Portfolio</a></li>
-		<li><a href="design-leadership.php">Design Leadership</a></li>
-		<li><a href="design-thinking.php">Design Thinking</a></li>
-		<li><a href="design-mentorship.php">Mentorship</a></li>
-		<li><a href="index.php#contact-section">Contact</a></li>
+		<li><a href="/index.php#portfolio-section">Portfolio</a></li>
+		<li><a href="/design-leadership.php">Design Leadership</a></li>
+		<li><a href="/design-thinking.php">Design Thinking</a></li>
+		<li><a href="/design-mentorship.php">Mentorship</a></li>
+		<li><a href="/index.php#contact-section">Contact</a></li>
 	<?	} ?>
 	</ul>
 	<div class="name-logo">
