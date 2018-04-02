afef12ceed1fd2da9bb2db548ca681f0f9d72420
John Murray <heyjohnmurray@us.ibm.com>
2018-04-02 00:39:53 -0500

    add beta

--- protected/ibm-microclimate.php
+++ protected/ibm-microclimate.php
@@ -174,69 +174,90 @@
 
 						<hr>
 
+						<h5>Creating an ad hoc product roadmap</h5>
+						<p>My product manager never had time to create a product roadmap in the early days. And there's nothing worse than trying to create something when you don't know what its long-term intent.</p>
+						<p>When I realized we were going to get a product roadmap anytime soon, I took it upon myself to create a living document of our end-to-end experience. I felt like if I had a sense of the current as-is scenario then I could show it to my stakeholders and have them prioritize what parts we want to fill in as we go along. Since it's a living document that I update, it serves as the ad hoc roadmap that I needed.</p>
+						<p>Take a look:</p>
+						<p><img src="/assets/images/screenshots/ibm-microclimate/process/as-is-scenario.png" alt=""></p>
+						<p>The image above is a zoomed in view of the end-to-end as-is scenario that I created early on and that I use as a proxy for a product roadmap. It allows me to keep my finger on the pulse of what's going on.</p>
+						<p><img src="/assets/images/screenshots/ibm-microclimate/process/as-is-scenario-gap.png" alt=""></p>
+						<p>In the image above you'll see a box with a dotted border. That's where I highlighted that we're missing functionality. By mapping out the end-to-end in this manner, I can objectively show our stakeholders where there are gaps in our product.</p>
+						<p><i><small>If you'd like to take a deeper look at this document you can <a href="/assets/images/screenshots/ibm-microclimate/process/microclimate-end-to-end-scenario.png">download it here</a>.</small></i></p>
 
+						<hr>
 
-						
-						
-						[reference the initial kickoff deck]
-						<p>After a few weeks of user research, stakeholder interviews, and competitive landscape analysis; we presented our findings to our leadership team as a way to reiterate and validate what we felt were the objectives so that we could move forward.</p>
-						<p>Based on the generative research we showed in that presentation, our leadership team agreed on a three-pronged mission. With the mission defined, we began to chart our course.</p>
-
+						<h5>Design Kickoff</h5>
+						<p>After weeks of user research, stakeholder interviews, and competitive landscape analysis; we presented our findings to our leadership team as a way to reiterate and validate what we felt were the objectives so that we could move forward.</p>
+						<p>Based on the generative research we showed in that presentation, our leadership team agreed that we need to focus on adding core functionality to the beta release, particularly focusing on "Getting Started".</p>
 					</div>
 				</div>
 			</div>
-
-
-
-			<h5>Strategic work</h5>
-			[key stakeholder/executive as source of truth]
-			[early stakeholder interviews to gauge alignment]
-			[developer surveys and interviewee recruiting]
-			[playback information at all-hands kickoff]
-			
-			<h4>Beta Release - January 15, 2018</h4>
-			[show peter screens]
-			[list feature objectives: getting started]
-
-			<h4>GA Release - March 30, 2018</h4>
-			[show GA screens]
-			[discuss q1 planning and use of story mapping]
-			[list feature objectives: import, pipeline]
-			[formal review - not-detailed]
-
-			<h4>Growing Pains and Learnings</h4>
-			[development restrictions and the impact on design]
-			[shared alignment with stakeholders and remote teams]
-			[creating a product while establishing workflows]
-			[establishing ux process is not instantaneous, it's progressive]
-			[not every feature is going to make it. you have to adjust]
-			[establish communication with stakeholders - design brief issue and sharing it out]
-			[avoiding an us vs them mentality] -->
-
-			<div class="callout">
-				<div class="row">
-					<div class="column small-12">
-						<h2>Callout HEADER</h2>
-						<p>CONTENT</p>
-						<!-- /.row -->
-					</div>
-					<!-- /.column -->
-				</div>
-				<!-- /.row -->
-			</div>
-			<!-- /.callout -->
 
 			<div class="body-copy">
 				<div class="row">
 					<div class="column small-12">
-						<h1>HEADER</h1>
-						<p>CONTENT</p>
+						<h4>Beta Release - January 15, 2018</h4>
+						<p>After we were aligned with stakeholders and agreed that the beta release needed to focus on "Getting Started", we dug deeper. After having meetings and working sessions, we agreed that a wizard-style project creation flow was the best experience. Users needed to be able to come in and create a microservice-based application quickly and easily.</p>
+						<p>Originally, our product was going to be non-IBM branded so as to appeal to developers who might be wary of trying something that looked and felt like "Big Blue".</p>
+						<p>Here are a few screenshots from our beta release:</p>
+						<p><img src="/assets/images/screenshots/ibm-microclimate/project-headshots/beta/welcome-get-started.png" alt=""></p>
+						<p><img src="/assets/images/screenshots/ibm-microclimate/project-headshots/beta/select-language.png" alt=""></p>
+						<p><img src="/assets/images/screenshots/ibm-microclimate/project-headshots/beta/confirmation.png" alt=""></p>
 					</div>
-					<!-- /.column -->
 				</div>
-				<!-- /.row -->
 			</div>
-			<!-- /.body-copy -->
+			
+			<div class="callout">
+				<div class="row">
+					<div class="column small-12">
+					<h4>GA Release - March 30, 2018</h4>
+						[show GA screens]
+						[discuss q1 planning and use of story mapping]
+						[list feature objectives: import, pipeline]
+						[formal review - not-detailed]
+					</div>
+				</div>
+			</div>
+
+			<div class="body-copy">
+				<div class="row">
+					<div class="column small-12">
+						<h4>Growing Pains and Learnings</h4>
+						<p>As productive as our team has been in the six months since we've started working together, it hasn't been without its share of setbacks. Here's a brief list of things I've learned along the way.</p>
+						
+						<h5>Development restrictions impact design</h5>
+						<p>You live and die by development's implementation schedule. No matter how much design work you hand over, if they hit an internal obstacle you may not see your work ship in the given release.</p>
+						<p>Secondly, keep in mind that a portion of your design resources will be dedicated to overseeing developmental implementation. So even though you want to hand off and move on to working on the next release, you're still going to need to remain diligent. Otherwise, you'll run into inconsistencies between the designs and the finished, shipped product.</p>
+					
+						<hr>
+
+						<h5>Shared alignment with stakeholders and remote teams</h5>
+						<p>When you have remote teams, you can't bring folks in to do Google-style design sprints. That simply doesn't work at IBM. I've learned to break up design sprints into smaller remote collaboration and ideation sessions. Every session comes with a scheduled review meeting where the output is shared with the larger team.</p>
+						<p><img src="/assets/images/screenshots/ibm-microclimate/process/ux-playback-process.png" alt=""></p>
+						<small>Notice in the image above that there are "playback" meetings scheduled all along the way.</small>
+						<hr>
+
+						<h5>Establishing UX process is not instantaneous, it's progressive</h5>
+						<p>I've <a href="https://medium.com/design-ibm/build-experiences-not-features-part-2-of-2-8d1c7ac523db">written extensively</a> on UX process and how to integrate design thinking with agile development. But showing the value of design and getting it a seat at the table take time. It's a slow and steady process and it's not something that is acheived in a single release or a single quarter. It's about refinement.</p>
+						<p><img src="/assets/images/screenshots/ibm-microclimate/process/ux-process.jpg" alt=""></p>
+						<small>I created the image above as part of a case study I did about how we can integrate design thinking and agile.</small>
+						
+						<hr>
+						
+						<h5>Be flexible in your communication</h5>
+						<p>While I'm a firm believer in over-communication when necessary, sometimes that can get tiring. Learn to change things up when your communication isn't resonating with folks.</p>
+						<p>Early on I was constantly being asked what design was going to deliver and when. Finally, I started to create a quarterly design brief in GitHub. I made it my top epic, and made all other epics dependencies of that parent epic. Then I shared out that link to the development leads and product management. Problem solved. Now I create a brief for every release and stakeholders have a better since of what my team is doing and delivering.</p>
+						<p><img src="/assets/images/screenshots/ibm-microclimate/process/github-design-brief.png" alt=""></p>
+						
+						<hr>
+						
+						<h5>You have to avoiding an "us vs. them" mentality</h5>
+						<p>It's all too easy to have an "us vs. them" mentality when you're dealing with team members on a multi-disciplinary product team. I've seen that type of negativity sink design's credibility all too often. I was so inspired by the idea that I <a href="https://www.youtube.com/watch?v=L2ZEp2wuH_o">wrote and gave a talk about it</a> at the UX Design Summit in San Francisco in March 2018.</p>
+
+						<hr>
+					</div>
+				</div>
+			</div>			
 		</div>
 		<!-- /.content -->
 	</section>
