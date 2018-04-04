4ebeb2451e7320c5b59495648b9a9709a2134b74
John Murray <heyjohnmurray@us.ibm.com>
2018-04-04 08:52:21 -0500

    update typos

--- protected/ibm-microclimate.php
+++ protected/ibm-microclimate.php
@@ -53,7 +53,7 @@
 
 			<div class="callout">
 				<div class="row">
-					<div class="column-12">
+					<div class="column small-12">
 						<h4>The First 30 Days</h4>
 						<p>As you read on the <a href="/ibm-microclimate.php">previous page</a> my team faced a number of hurdles early on including:</p>
 						<ol class="list">
@@ -109,91 +109,100 @@
 					
 						<hr>
 
-						<h5>Onboarding and level-setting as a team</h5>
-						<p>After I read everything I could get my hands on I set up daily knowledge sharing meetings with my design team to explain what I'd learned and to assign them a topic to go out and do further investigation on to share with the group.</p>
-						<p>Despite our constant knowledge sharing, some folks were still struggling to make heads or tails of the rather dense subject matter. Understandable, so I created a document diagraming everything I knew and walked them through it.</p>
-						<p>One of the hardest parts about being a leader in these situations is sharing what you know at the same rate at which you're learning it. To help me with that I also diagrammed the ongoing business strategy and a 30/60/90 outlook for the design team so they didn't feel out-of-the-loop.</p>
-						
-						<p><img src="/assets/images/screenshots/ibm-microclimate/process/knowledge-gathering.png" alt=""></p>
-						<p><img src="/assets/images/screenshots/ibm-microclimate/process/knowledge-gathering2.png" alt=""></p>
-						
-						<p>As we were learning about the domain and meeting with stakeholders, I wanted to be open with my team about what we knew, what we didn't know, and what we were afraid to ask.</p>
-						<p><img src="/assets/images/screenshots/ibm-microclimate/process/questions-and-assumptions.png" alt=""></p>
+						<div class="row column small-12">
+							<h5>Onboarding and level-setting as a team</h5>
+							<p>After I read everything I could get my hands on I set up daily knowledge sharing meetings with my design team to explain what I'd learned and to assign them a topic to go out and do further investigation on to share with the group.</p>
+							<p>Despite our constant knowledge sharing, some folks were still struggling to make heads or tails of the rather dense subject matter. Understandable, so I created a document diagraming everything I knew and walked them through it.</p>
+							<p>One of the hardest parts about being a leader in these situations is sharing what you know at the same rate at which you're learning it. To help me with that I also diagrammed the ongoing business strategy and a 30/60/90 outlook for the design team so they didn't feel out-of-the-loop.</p>
+							
+							<p><img src="/assets/images/screenshots/ibm-microclimate/process/knowledge-gathering.png" alt=""></p>
+							<p><img src="/assets/images/screenshots/ibm-microclimate/process/knowledge-gathering2.png" alt=""></p>
+							
+							<p>As we were learning about the domain and meeting with stakeholders, I wanted to be open with my team about what we knew, what we didn't know, and what we were afraid to ask.</p>
+							<p><img src="/assets/images/screenshots/ibm-microclimate/process/questions-and-assumptions.png" alt=""></p>
 
-						<small><i>At the time the product only had a working name, Microclimate, so I was just calling it "TBD Offering".</i></small>
-						<p><img src="/assets/images/screenshots/ibm-microclimate/process/early-strategy-insights1.png" alt=""></p>
-						<p><img src="/assets/images/screenshots/ibm-microclimate/process/early-strategy-insights3.png" alt=""></p>
-						<p><img src="/assets/images/screenshots/ibm-microclimate/process/early-strategy-insights2.png" alt=""></p>
-						
-						<hr>
+							<small><i>At the time the product only had a working name, Microclimate, so I was just calling it "TBD Offering".</i></small>
+							<p><img src="/assets/images/screenshots/ibm-microclimate/process/early-strategy-insights1.png" alt=""></p>
+							<p><img src="/assets/images/screenshots/ibm-microclimate/process/early-strategy-insights3.png" alt=""></p>
+							<p><img src="/assets/images/screenshots/ibm-microclimate/process/early-strategy-insights2.png" alt=""></p>
+							
+							<hr>
 
-						<h5>Defining the user</h5>
-						<p>While I was learning and sharing with my team, I worked with our researcher to do some initial surveys and developer interviews.</p>
-						
-						<p>We asked questions a number of questions including:</p>
-						<ul class="list">
-							<li>How do developers discover, try and buy new tooling?</li>
-							<li>What traits did they value most when evaluating new tooling?</li>
-							<li>Where does IBM stand in the developer community?</li>
-							<li>Are developers brand loyal to any single cloud platform?</li></li>
-						</ul>
+							<h5>Defining the user</h5>
+							<p>While I was learning and sharing with my team, I worked with our researcher to do some initial surveys and developer interviews.</p>
+							
+							<p>We asked a number of questions including:</p>
+							<ul class="list">
+								<li>How do developers discover, try and buy new tooling?</li>
+								<li>What traits do they value most when evaluating new tooling?</li>
+								<li>Where does IBM stand in the developer community?</li>
+								<li>Are developers brand loyal to any single cloud platform?</li></li>
+							</ul>
 
-						<p>Here's a summary of those early research findings.</p>
-						<iframe src="//www.slideshare.net/slideshow/embed_code/key/pvsKgcoHFZF8iC" width="595" height="485" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="border:1px solid #CCC; border-width:1px; margin-bottom:5px; max-width: 100%;" allowfullscreen></iframe>
+							<p>Here's a summary of those early research findings:</p>
+							<iframe src="//www.slideshare.net/slideshow/embed_code/key/pvsKgcoHFZF8iC" width="595" height="485" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="border:1px solid #CCC; border-width:1px; margin-bottom:5px; max-width: 100%;" allowfullscreen></iframe>
 
-						<p>While we were doing surveys and interviews, we looked at personas we may have inherited with the three products that were merging to form our new product team. Microservice Builder had a couple of personas, and after some discussions with our stakeholders settled on Rohan, the devops developer.</p>
-						<p>We chose Rohan because he was validated by previous researchers on our design org and because he matched the non-traditional IBM customer our stakeholders had in mind.</p>
+							<p>While we were doing surveys and interviews, we looked at personas we may have inherited with the three products that were merging to form our new product team. Microservice Builder had a couple of personas, and after some discussions with our stakeholders settled on Rohan, the devops developer.</p>
+							<p>We chose Rohan because he was validated by previous researchers on our design org and because he matched the non-traditional IBM customer our stakeholders had in mind.</p>
 
-						<h5>Rohan, the devops developer</h5>
-						<small>Summary</small>
-						<p>Rohan works in a smaller company in a medium size city. His role as a developer requires him to wear many hats managing both the customer facing applications and operations for the company. His skills are similar to Maureen (Developer) but their differing work environments and team structure means they have unique needs.</p>
+							<h5>Rohan, the devops developer</h5>
+							<small>Summary</small>
+							<p>Rohan works in a smaller company in a medium size city. His role as a developer requires him to wear many hats managing both the customer facing applications and operations for the company. His skills are similar to Maureen (Developer) but their differing work environments and team structure means they have unique needs.</p>
 
-						<small>Goals</small>
-						<ul class="list">
-							<li>Create, deploy and manage applications and microservices</li>
-							<li>Find affordable tools and services to simplify his operations workflow</li>
-						</ul>
+							<small>Goals</small>
+							<ul class="list">
+								<li>Create, deploy and manage applications and microservices</li>
+								<li>Find affordable tools and services to simplify his operations workflow</li>
+							</ul>
 
-						<small>Behaviors</small>
-						<ul class="list">
-							<li>Actively participates in local professional groups and meetups</li>
-							<li>Brings his work home with him</li>
-						</ul>
+							<small>Behaviors</small>
+							<ul class="list">
+								<li>Actively participates in local professional groups and meetups</li>
+								<li>Brings his work home with him</li>
+							</ul>
 
-						<small>Beliefs</small>
-						<ul class="list">
-							<li>He'd rather pay for a service to maintain an app so he can focus on the app</li>
-							<li>Open source is great</li>
-						</ul>
+							<small>Beliefs</small>
+							<ul class="list">
+								<li>He'd rather pay for a service to maintain an app so he can focus on the app</li>
+								<li>Open source is great</li>
+							</ul>
 
-						<small>Needs</small>
-						<ul class="list">
-							<li>To find affordable tools and services to simplify his operations workflow</li>
-							<li>Guidance for cloud native best practices</li>
-						</ul>
+							<small>Needs</small>
+							<ul class="list">
+								<li>To find affordable tools and services to simplify his operations workflow</li>
+								<li>Guidance for cloud native best practices</li>
+							</ul>
 
-						<hr>
+							<hr>
 
-						<h5>Creating an ad hoc product roadmap</h5>
-						<p>My product manager never had time to create a product roadmap in the early days. And there's nothing worse than trying to create something when you don't know what its long-term intent.</p>
-						<p>When I realized we were going to get a product roadmap anytime soon, I took it upon myself to create a living document of our end-to-end experience. I felt like if I had a sense of the current as-is scenario then I could show it to my stakeholders and have them prioritize what parts we want to fill in as we go along. Since it's a living document that I update, it serves as the ad hoc roadmap that I needed.</p>
-						<p>Take a look:</p>
-						<p><img src="/assets/images/screenshots/ibm-microclimate/process/as-is-scenario.png" alt=""></p>
-						<p>The image above is a zoomed in view of the end-to-end as-is scenario that I created early on and that I use as a proxy for a product roadmap. It allows me to keep my finger on the pulse of what's going on.</p>
-						<p><img src="/assets/images/screenshots/ibm-microclimate/process/as-is-scenario-gap.png" alt=""></p>
-						<p>In the image above you'll see a box with a dotted border. That's where I highlighted that we're missing functionality. By mapping out the end-to-end in this manner, I can objectively show our stakeholders where there are gaps in our product.</p>
-						<p><a href="/assets/images/screenshots/ibm-microclimate/process/microclimate-end-to-end-scenario.png" class="button is-primary">Click to view artifact!</a></p>
-
-						<hr>
-
-						<h5>Design Kickoff</h5>
-						<p>After weeks of user research, stakeholder interviews, and competitive landscape analysis; we presented our findings to our leadership team as a way to reiterate and validate what we felt were the objectives so that we could move forward.</p>
-						<p>Based on the generative research we showed in that presentation, our leadership team agreed that we need to focus on adding core functionality to the beta release, particularly focusing on "Getting Started".</p>
+							<h5>Creating an ad hoc product roadmap</h5>
+							<p>My product manager never had time to create a product roadmap in the early days. And there's nothing worse than trying to create something when you don't know the long-term strategy.</p>
+							<p>When I realized we weren't going to get a product roadmap anytime soon, I took it upon myself to create a living document of our end-to-end experience. I felt like if I had a sense of the current as-is scenario then I could show it to my stakeholders and have them prioritize what parts we want to fill in as we go along. Since it's a living document that I update, it has served as the ad hoc roadmap that I need.</p>
+							<p>Take a look:</p>
+							<p><img src="/assets/images/screenshots/ibm-microclimate/process/as-is-scenario.png" alt=""></p>
+							<p>The image above is a zoomed in view of the end-to-end as-is scenario that I created early on and that I use as a proxy for a product roadmap. It allows me to keep my finger on the pulse of what's going on.</p>
+							<p><img src="/assets/images/screenshots/ibm-microclimate/process/as-is-scenario-gap.png" alt=""></p>
+							<p>In the image above you'll see a box with a dotted border. That's where I highlighted that we're missing functionality. By mapping out the end-to-end in this manner, I can objectively show our stakeholders where there are gaps in our product.</p>
+							<p><a href="/assets/images/screenshots/ibm-microclimate/process/microclimate-end-to-end-scenario.png" class="button is-primary">Click to view artifact!</a></p>
+						</div>
 					</div>
 				</div>
 			</div>
 
 			<div class="body-copy">
+				<div class="row">
+					<div class="column small-12">
+						<h4>Design Kickoff</h4>
+						<p>After weeks of user research, stakeholder interviews, and competitive landscape analysis; we presented our findings to our leadership team as a way to reiterate and validate what we felt were the objectives so that we could move forward.</p>
+						<p>Based on the generative research we showed in that presentation, our leadership team agreed that we need to focus on adding core functionality to the beta release, particularly focusing on "Getting Started".</p>
+						<p>Here's the two-part presentation we gave that day. The first one is the overall presentation, and the second is, once again, the summary of the research findings.</p>
+						<iframe src="//www.slideshare.net/slideshow/embed_code/key/7QsoueEz5eHJMP" width="595" height="485" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="border:1px solid #CCC; border-width:1px; margin-bottom:5px; max-width: 100%;" allowfullscreen></iframe>
+						<iframe src="//www.slideshare.net/slideshow/embed_code/key/pvsKgcoHFZF8iC" width="595" height="485" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="border:1px solid #CCC; border-width:1px; margin-bottom:5px; max-width: 100%;" allowfullscreen></iframe>
+					</div>
+				</div>
+			</div>
+
+			<div class="callout">
 				<div class="row">
 					<div class="column small-12">
 						<h4>Beta Release - January 15, 2018</h4>
@@ -207,7 +216,7 @@
 				</div>
 			</div>
 			
-			<div class="callout">
+			<div class="body-copy">
 				<div class="row">
 					<div class="column small-12">
 						<h4>GA Release - March 30, 2018</h4>
@@ -249,9 +258,9 @@
 						<hr>
 						<h5>Storyboarding</h5>
 						<p>With loose user stories from the sotry mapping session, I was able to write a user journey storyline that I shared with one of our designers, and he was able to begin to create a storyboard from what I shared with him.</p>
-						<a href="https://ibm.invisionapp.com/share/HRFFTLLP8ZK" class="button is-primary">Click to view prototype!</a>			
-						<img src="/assets/images/screenshots/ibm-microclimate/process/usm-storyboards.jpg" alt="">
-						<img src="/assets/images/screenshots/ibm-microclimate/process/usm-storyboards2.jpg" alt="">
+						<p><a href="https://ibm.invisionapp.com/share/HRFFTLLP8ZK" class="button is-primary">Click to view prototype!</a></p>
+						<p><img src="/assets/images/screenshots/ibm-microclimate/process/usm-storyboards.jpg" alt=""></p>
+						<p><img src="/assets/images/screenshots/ibm-microclimate/process/usm-storyboards2.jpg" alt=""></p>
 						<hr>
 						<h5>Low-fi wireframes</h5>
 						<p>With the stories defined, we began to create low-fi wireframes to further evolve the user journey as we understood it.</p>
@@ -279,7 +288,7 @@
 				</div>
 			</div>
 
-			<div class="body-copy">
+			<div class="callout">
 				<div class="row">
 					<div class="column small-12">
 						<h4>Growing Pains and Learnings</h4>
@@ -311,7 +320,7 @@
 						<p><img src="/assets/images/screenshots/ibm-microclimate/process/ux-process-v1.png" alt=""></p>
 						<p><a href="/assets/images/screenshots/ibm-microclimate/process/ux-process-v1.png" class="button is-primary">Click to view artifact!</a></p>
 
-						<p>Ideally, we're trying to evolve to something more efficient like what you see in this more developed inforgraphic below. Slowly but surely we're getting there.</p>
+						<p>Ideally, we're trying to evolve to something more efficient like what you see in this inforgraphic below. Slowly but surely we're getting there.</p>
 						<p><img src="/assets/images/screenshots/ibm-microclimate/process/ux-process.jpg" alt=""></p>
 						<small>I created the image above as part of a case study I did about how we can integrate design thinking and agile.</small>
 						
